9f33fd419d0bb536ddd2249969265932e8b2af4d
Farshad Rajabi <farshad.rajabi.33@gmail.com>
2025-03-13 21:06:49 +0100

    Add GitHub Actions CI for Build & Test

--- backend/user-service/src/main/java/com/example/userservice/FriendalyzeApplication.java
+++ backend/user-service/src/main/java/com/example/userservice/FriendalyzeApplication.java
@@ -1,5 +1,5 @@
 
-package com.example.friendalyze;
+package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
